3d99a975a09fcec25f55963c0cc6b08e1c43d377
srisanth.e9@gmail.com <srisanth.e9@gmail.com>
2024-10-01 09:36:43 +0530

    adding changes

--- code.c
+++ code.c
@@ -1,1 +1,10 @@
-printf("hello world")
+#include<stdio.h>
+
+void main(){
+int n,sum=0;
+printf("enter n:");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{sum=sum+i;}
+printf("\nsum of natural %d numbers is:%d",n,sum);
+}
